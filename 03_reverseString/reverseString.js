e0304a6e6df92094b3d1abf2d775eef0d8e38e2c
muzard <arttu.kokki@gmail.com>
2022-08-13 21:43:21 +0300

    Complete exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+function reverseString(reverseString) {
+    let listOfChars = reverseString.split("")
 
+    let reverseList = []
+
+    for (let i = listOfChars.length; i >= 0; i--) {
+        reverseList.push(listOfChars[i])
+    }
+    return reverseList.join("")
 };
 
 // Do not edit below this line
