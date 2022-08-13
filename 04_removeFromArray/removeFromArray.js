44e8aa18320edd846c6529cc24a3286084217e25
muzard <arttu.kokki@gmail.com>
2022-08-13 22:05:59 +0300

    Complete exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(list) {
+    const args = [...arguments];
+    args.splice(0, 1)
 
+    returnables = []
+
+    for (let i = 0; i < list.length; i++) {
+        if (!(args.indexOf(list[i]) >= 0)) {
+            returnables.push(list[i])
+        }
+    }
+    return returnables
 };
 
 // Do not edit below this line
