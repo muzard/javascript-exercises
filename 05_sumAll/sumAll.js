e7082c45d3799024f9ea2a7f247c72dd01765402
muzard <arttu.kokki@gmail.com>
2022-08-13 22:18:53 +0300

    Complete exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(numOne, numTwo) {
+    let finalValue = 0;
+    let a;
+    let b;
 
+    if (typeof(numOne) != "number" || typeof(numTwo) != "number") return "ERROR";
+    if (numOne < 0 || numTwo < 0) return "ERROR";
+
+    if (numOne < numTwo) {
+        a = numOne
+        b = numTwo
+    } else {
+        a = numTwo
+        b = numOne
+    }
+
+    for (a; a <= b; a++) {
+        finalValue += a
+    }
+    return finalValue;
 };
 
 // Do not edit below this line
