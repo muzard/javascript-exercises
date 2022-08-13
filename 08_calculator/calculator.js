c5c37b8faea5734524efea6873059ddaff883b64
muzard <arttu.kokki@gmail.com>
2022-08-13 23:43:47 +0300

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(numList) {
+	let sum = 0
+
+  for (const num of numList) {
+    sum += num
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(numList) {
+  product = 1
+  for (const num of numList) {
+    product *= num
+  }
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let result = 1;
+  for (let i = 1; i <= num; i++) {
+    result *= i
+  }
+  return result
 };
 
 // Do not edit below this line
