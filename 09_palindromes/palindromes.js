5d28dcda2ac22980d01f107f5daab418097b8a9c
muzard <arttu.kokki@gmail.com>
2022-08-14 12:05:53 +0300

    Complete exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase().split("");
+    let returnable = [];
+    let newStr = [];
+    const noToThis = [",","!"," ","."]
 
+    for (const char of str) {
+        if (noToThis.indexOf(char) >= 0) continue;
+        returnable.push(char)
+        newStr.push(char)
+    }
+    returnable.reverse()
+    return (returnable.join() == newStr.join());
 };
 
 // Do not edit below this line
