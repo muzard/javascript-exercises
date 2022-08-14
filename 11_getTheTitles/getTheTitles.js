45b6267cd41d09e9897dff54ae74ac2078490c50
muzard <arttu.kokki@gmail.com>
2022-08-14 15:21:31 +0300

    Complete exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,9 @@
-const getTheTitles = function() {
-
-};
-
+const getTheTitles = function(array) {
+    let returnables = [];
+    for (const dict of array) {
+        returnables.push(dict["title"])
+    }
+    return returnables
+}
 // Do not edit below this line
 module.exports = getTheTitles;
