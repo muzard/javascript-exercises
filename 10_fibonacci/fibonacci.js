07b5d88d8ff90b6adad6241a89e9309469477b06
muzard <arttu.kokki@gmail.com>
2022-08-14 15:11:26 +0300

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    n = parseInt(n)
+    const start = [1, 1]
+    for (let i = 2; i < n; i++) {
+        start.push(start[i-1] + start[i-2])
+    }
+    return start[start.length - 1]
 };
 
 // Do not edit below this line
