41f61147a01490bf7b67d52a36cdf1c434b05e3e
muzard <arttu.kokki@gmail.com>
2022-09-15 00:27:54 +0300

    complete exercise findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+function getAge(person) {
+    age = 0
+    if (!person.yearOfDeath) {
+        age = new Date().getFullYear() - person.yearOfBirth
+    } else {
+        age = person.yearOfDeath - person.yearOfBirth
+    }
+    return age;
+}
 
+const findTheOldest = function(arr) {
+    let oldestPerson = arr[0];
+    for (let key in arr) {
+        age = getAge(arr[key])
+        if (age > getAge(oldestPerson)) {
+            oldestPerson = arr[key]
+        }
+    }
+    return oldestPerson;
 };
 
 // Do not edit below this line
